2121a061bde5668c82ef41d3c65d7b10a1f3d835
Nancy Rackleff <nancyrackleff@gmail.com>
2014-04-01 16:18:11 -0700

    Initial commit
    Completed todo app, library app WIP.

---
+++ library/site/js/views/book.js
@@ -0,0 +1,18 @@
+// site/js/views/book.js
+
+var app = app || {};
+
+$(function() {
+  app.BookView = Backbone.View.extend({
+    tagName: 'div',
+    className: 'bookContainer',
+    template: _.template( $('#bookTemplate').html() ),
+
+    render: function() {
+      // template is a function that takes a JSON object and returns HTML
+      this.$el.html( this.template( this.model.toJSON() ) );
+
+      return this;
+    }
+  });
+});
---
+++ todo/js/routers/router.js
@@ -0,0 +1,22 @@
+// js/routers/router.js
+
+// Todo router
+// -----------
+
+var Workspace = Backbone.Router.extend({
+  routes:{
+    '*filter': 'setFilter'
+  },
+
+  setFilter: function( param ) {
+    // Set the current filter to be used
+    app.TodoFilter = param || '';
+
+    // Trigger a collection filter event, causing hiding/unhiding
+    // of Todo view items
+    app.Todos.trigger('filter');
+  }
+});
+
+app.TodoRouter = new Workspace();
+Backbone.history.start();
