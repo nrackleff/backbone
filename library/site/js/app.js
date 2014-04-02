6878621b6ca4c9aa1b0046abfad47e5a83ae12fe
Nancy Rackleff <nancyrackleff@gmail.com>
2014-04-02 15:54:37 -0700

    Library app complete

--- library/site/js/app.js
+++ library/site/js/app.js
@@ -3,13 +3,6 @@
 var app = app || {};
 
 $(function() {
-  var books = [
-    { title: 'A Book', author: 'A Person', releaseDate: '2010', keywords: 'Some Words'},
-    { title: 'Star Trek', author: 'A Geek', releaseDate: '2014', keywords: 'Some Words'},
-    { title: 'Catcher in the Rye', author: 'A Recluse', releaseDate: '2010', keywords: 'Some Words'},
-    { title: 'Another Book', author: 'A Person', releaseDate: '2010', keywords: 'Some Words'},
-    { title: 'Yet Another Book', author: 'A Person', releaseDate: '2010', keywords: 'Some Words'},
-  ];
-
-  new app.LibraryView( books );
+	$('#releaseDate').datepicker();
+  	new app.LibraryView();
 });
--- library/site/js/views/library.js
+++ library/site/js/views/library.js
@@ -8,8 +8,10 @@ $(function() {
 
     initialize: function( initialBooks ) {
       this.collection = new app.Library( initialBooks );
+      this.collection.fetch({reset: true});
       this.render();
       this.listenTo(this.collection, 'add', this.renderBook);
+      this.listenTo(this.collection, 'reset', this.render);
     },
 
     render: function() {
@@ -36,11 +38,35 @@ $(function() {
       var formData = {};
       $( '#addBook div' ).children( 'input' ).each( function( i, el ) {
         if( $(el).val() != '' ) {
-          formData[ el.id ] = $(el).val();
+          if(el.id === 'keywords'){
+            formData[el.id] = [];
+            _.each($(el).val().split(' '), function( keyword ) {
+              formData[el.id].push({'keyword': keyword});
+            });
+          } else if (el.id === 'releaseDate') {
+            formData[el.id] = $('#releaseDate').datepicker('getDate').getTime();
+          } else {
+            formData[el.id] = $(el).val();
+          }
         }
+        // Clear input field value
+        $(el).val('');
       });
 
-      this.collection.add( new app.Book(formData));
+      this.collection.create(formData);
     }
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
